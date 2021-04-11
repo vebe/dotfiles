f5da16a3babaecef5d4d44e9915e2e7a63861663
Victor Blessenius <blessenius@gmail.com>
2021-04-11 13:32:42 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 USERNAME=$(id -un)
-DOTFILESPATH=$(dirname $(realpath $0))
+DOTFILESPATH="$(realpath $0)"
+DOTFILESPATH="$(dirname "$DOTFILESPATH")"
 
 echo "Installing Oh My Zsh config"
-ln -nfs $DOTFILESPATH/.zshrc /Users/$USERNAME/.zshrc
+ln -nfs "$DOTFILESPATH/.zshrc" "/Users/$USERNAME/.zshrc"
+echo "Symlink from $DOTFILESPATH/.zshrc to /Users/$USERNAME/.zshrc added"
 
 echo "Done!"
